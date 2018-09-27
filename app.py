99134eb8267d6fb9e170c2e27b52a34851833e2f
luisAr28 <luisalfonso_rp28@hotmail.com>
2018-09-26 22:59:59 -0500

    Funcional
    pre instalación en el CIC

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template,jsonify,request,redirect,url_for,json
+from flask import Flask, render_template,jsonify,request,redirect,url_for,json,Response,send_file,send_from_directory
+from datetime import datetime
+from werkzeug.datastructures import ImmutableMultiDict
+from pprint import pprint
 app = Flask(__name__)
 
 @app.route('/')
@@ -20,6 +23,10 @@ def processjson():
 def prueba():
     return "/tagging"
 
+@app.route('/etiquetado',methods=['POST'])
+def etiquetado():
+    return "/loadTag"
+
 
 @app.route("/reqF",methods=['POST'])
 def signF():
@@ -38,19 +45,162 @@ def signF():
 	else:
 		return json.dumps({'html':'<span>Faltan !!</span>'})
 
-@app.route("/ejemplo",methods=['POST'])
-def ejemplo():
+@app.route("/fillF",methods=['POST'])
+def fillF():
+	name= request.form.get('nombre')
+	rout= request.form.get('ruta')
+	nImgs= request.form.get('nImags')
+	orAlet= request.form.get('ordAlet')
 	questions = int(request.form.get('nQuest'))
-	print(questions)
-	return render_template('tag.html',questions=questions)
+	if orAlet:
+		valAlet="1"
+		orAlet="checked"
+	else:
+		orAlet="unchecked"
+		valAlet="0"
+	print(orAlet)
+	#if name and rout and nImags and questions:
+	return render_template('otro.html',questions=questions,nameI=name,rout=rout,nImgs=nImgs,orAlet=orAlet,valAlet=valAlet)
+	#else
+
 
 @app.route("/saveJson",methods=['POST'])
 def saveJson():
-    print("Si")
+    now = datetime.now()
+    complete = str(now.year)+str(now.month)+str(now.day)
+    dat = request.form
+    dat.to_dict(flat=False)
+    #otro = str(request.form.get('nombre'))
+    otro = str()
+    otro = otro+complete
+   
+    print(complete)
+    print(dat)
+    print(otro)
+   
+   #createfolder = os.path.join('C:/Users/MyUser/Desktop/Project/', 'Fileuploads/', time_stamp,)
+    #filename = request.form.get('nombre')
+    #with open(filename, 'w') as f:
+     #   json.dump(request.form, f)
+    #return '/'
+    return json.dumps({'urlI':'/'})
 
 @app.route('/tagging')
 def tagging():
-       return render_template('tag.html');
+       return render_template('tag.html')
+    
+@app.route('/loadTag')
+def loadTag():
+       return render_template('lTag.html')
+    
+@app.route('/jForm',methods=['POST']) 
+def jForm():
+    file = request.files['fileJson']
+    #filename = secure_filename(file.filename) 
+    #file.save(os.path.join(SITE_ROOT, "static/temp",filename))
+    #filetype = magic.from_buffer(file.read())
+    jsonInfo = file.read()
+    jsonInfo = jsonInfo.decode('utf-8')
+    jsonInfo2 = json.loads(jsonInfo)
+    hists = os.listdir(os.path.join(app.static_folder,jsonInfo2['category1']['path']))
+    hists = [jsonInfo2['category1']['path']+'/' + file for file in hists]
+    #list = []
+    #for i in range(jsonInfo2['category1']['quantity']):
+    #      r=random.randint(1,100)
+    #      if r not in list: list.append(r)
+            
+    print('\n'+jsonInfo)
+    print(jsonInfo2['category1']['name'])
+    
+    return render_template('jsonForm.html',jsonInfo2=jsonInfo2,hists=hists)
+
+@app.route('/jsonDat',methods=['POST']) 
+def jsonDat():
+    print("entrando")
+    dat = request.form.get('opt1')
+    prueba = request.form.get('text1')
+   # dat.to_dict(flat=False)
+#    dato = request.get_json()
+    dat = request.form
+    #dat = dict(request.form)
+   # print(dato)
+    print(dat)
+    print(prueba)
+    
+    dataJ = "{"
+    
+    topQuest = request.form.get('preguntas',type=int)
+
+    totalIm = request.form.get('cantidad',type=int)
+
+    questPerIm =  topQuest / totalIm
+    contIm = 1
+    contClass =1
+
+    dataJ = dataJ+obtieneNomImag(str(request.form.get('img'+str(contIm))))+":[{"
+
+    for i in range(0,topQuest):
+        dataJ = dataJ + "Classname"+str(contClass)+":{opt:"+ str(request.form.get('opt'+str(i+1)))+",text:"+str(request.form.get('text'+str(i+1)))+"}"
+
+        if (i+1)==(contIm*questPerIm) and (i+1)!=topQuest:      
+            contIm+=1
+            contClass=1
+            dataJ=dataJ+"}],"+obtieneNomImag(str(request.form.get('img'+str(contIm))))+":[{"
+        else:
+            if (i+1)!=topQuest:
+                dataJ=dataJ+","
+                contClass+=1
+        
+    
+   # for i in range(request.form.get('numberQ')):
+    #    dataJ = dataJ + request.form.get('img{i+1}')
+    
+    dataJ = dataJ+"}}"
+    
+    print("Es: "+dataJ)
+    
+    pathName = obtieneNomImag(str(request.form.get('path')))
+    pathFile = "static/temp/"+pathName+".json"
+    
+    with open(pathFile, 'w') as outfile:
+        outfile.write(dataJ)
+    
+    #fileSen = open(pathFile,'rb').read()
+    
+    
+    #response.headers['Content-Type'] = 'application/json'
+    #response.header['Content-Disposition'] = 'attatchment;filename='+pathName+'.json'
+    
+    #return response
+    #print(Response(dataJ, mimetype='application/json',headers={'Content-Disposition':'attachment;filename=file.json'}))
+    
+    #return Response(dataJ, mimetype='application/json',headers={'Content-Disposition':'attachment;filename=file.json'})
+    #response = Response(response=dataJ, status=200, mimetype="application/json")
+    #return response
+    #return send_file(pathFile, as_attachment=True)
+    #return send_from_directory(directory='/static/temp', filename=pathName+'.json', as_attachment=True)
+    #return send_file(json.dumps(dataJ), mimetype='application/json',attachment_filename='file.json',as_attachment=True)
+    #return json.dumps({'urlI':'/downloadF','name':pathName})
+    return json.dumps({'urlI':'/'})
+    #return render_template('downJson.html');
+
+#@app.route('/sendFile', methods=['POST'])
+#def sendFile():
+#    content = str(request.form['jsonval'])
+#    return Response(content, 
+#            mimetype='application/json',
+#            headers={'Content-Disposition':'attachment;filename=file.json'})
+
+@app.route('/downloadF/<path_file>')
+def downloadF(path_file):
+    print(path_file)
+    return render_template('downJson.html',nFile='temp/'+path_file+'.json')
+
+def obtieneNomImag(rutIm):
+    index = rutIm.rfind('/')
+    nom = rutIm[index+1:]
+    
+    return nom
     
 if __name__=="__main__":
     app.run(debug = True)
